dd63bf3274ec7ebb93b6afd854640fbe928cbec7
Benedikt Michel <benemichel_code@posteo.de>
2021-10-05 16:43:50 +0200

    SAPL integration

--- src/main/java/com/example/axondemo/command/BankAccount.java
+++ src/main/java/com/example/axondemo/command/BankAccount.java
@@ -2,15 +2,20 @@ package com.example.axondemo.command;
 
 import com.example.axondemo.command.coreapi.AccountCreatedEvent;
 import com.example.axondemo.command.coreapi.CreateAccountCommand;
+import io.sapl.api.pdp.PolicyDecisionPoint;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.axonframework.commandhandling.CommandHandler;
 import org.axonframework.eventsourcing.EventSourcingHandler;
+import org.axonframework.messaging.InterceptorChain;
 import org.axonframework.modelling.command.AggregateIdentifier;
 import org.axonframework.modelling.command.AggregateLifecycle;
+import org.axonframework.modelling.command.CommandHandlerInterceptor;
 import org.axonframework.spring.stereotype.Aggregate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 
@@ -27,10 +32,20 @@ public class BankAccount {
     private String id;
     private int deposit;
 
+
+
+
     @CommandHandler
     public BankAccount(CreateAccountCommand command) {
+
         AggregateLifecycle.apply(new AccountCreatedEvent(command.getId(), command.getDeposit()));
     }
+//
+//    @CommandHandlerInterceptor
+//    public void intercept(CreateAccountCommand command, InterceptorChain interceptorChain) {
+//
+//
+//    }
 
     @EventSourcingHandler
     public void on(AccountCreatedEvent event) {
