adc999c5710b6c07a21fcb68b0070bf6a4659474
Benedikt Michel <benemichel_code@posteo.de>
2021-10-05 16:09:09 +0200

    account doc projection

--- src/main/java/com/example/axondemo/command/BankAccount.java
+++ src/main/java/com/example/axondemo/command/BankAccount.java
@@ -11,12 +11,16 @@ import org.axonframework.eventsourcing.EventSourcingHandler;
 import org.axonframework.modelling.command.AggregateIdentifier;
 import org.axonframework.modelling.command.AggregateLifecycle;
 import org.axonframework.spring.stereotype.Aggregate;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+
+
+@Data
+@Slf4j
+@Document
 @Aggregate
 @NoArgsConstructor
 @AllArgsConstructor
-@Slf4j
-@Data
 public class BankAccount {
 
     @AggregateIdentifier
