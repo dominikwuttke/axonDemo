cbf90a837a98659ed1f66c4b715f54f5eb24ef1e
Dominik Wuttke <Dominik.wu@gmx.net>
2021-10-06 15:51:26 +0200

    Tokenstore hinzugefügt

--- src/main/java/com/example/axondemo/MyAxonConfiguration.java
+++ src/main/java/com/example/axondemo/MyAxonConfiguration.java
@@ -2,18 +2,31 @@ package com.example.axondemo;
 
 
 import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoDatabase;
 import io.sapl.api.pdp.PolicyDecisionPoint;
 import lombok.extern.slf4j.Slf4j;
 import org.axonframework.commandhandling.CommandBus;
 import org.axonframework.commandhandling.SimpleCommandBus;
+import org.axonframework.config.EventProcessingConfigurer;
+import org.axonframework.eventhandling.TrackingEventProcessorConfiguration;
+import org.axonframework.eventhandling.tokenstore.TokenStore;
+import org.axonframework.eventhandling.tokenstore.jpa.JpaTokenStore;
 import org.axonframework.eventsourcing.eventstore.EmbeddedEventStore;
 import org.axonframework.eventsourcing.eventstore.EventStorageEngine;
 import org.axonframework.eventsourcing.eventstore.EventStore;
 import org.axonframework.extensions.mongo.DefaultMongoTemplate;
+import org.axonframework.extensions.mongo.MongoTemplate;
 import org.axonframework.extensions.mongo.eventsourcing.eventstore.MongoEventStorageEngine;
+import org.axonframework.extensions.mongo.eventsourcing.tokenstore.MongoTokenStore;
+import org.axonframework.messaging.StreamableMessageSource;
+import org.axonframework.serialization.Serializer;
+import org.axonframework.serialization.xml.XStreamSerializer;
 import org.axonframework.spring.config.AxonConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Configuration
@@ -36,6 +49,39 @@ public class MyAxonConfiguration {
                 .build();
     }
 
+    @Autowired
+    public void configureInitialTrackingToken(EventProcessingConfigurer processingConfigurer) {
+        TrackingEventProcessorConfiguration tepConfig =
+                TrackingEventProcessorConfiguration.forSingleThreadedProcessing()
+                        .andInitialTrackingToken(StreamableMessageSource::createHeadToken);
+
+        processingConfigurer.registerTrackingEventProcessorConfiguration(config -> tepConfig);
+    }
+
+    @Autowired
+    public void configureTokenClaimValues(EventProcessingConfigurer processingConfigurer) {
+        TrackingEventProcessorConfiguration tepConfig =
+                TrackingEventProcessorConfiguration.forSingleThreadedProcessing()
+                        .andTokenClaimInterval(1000, TimeUnit.MILLISECONDS)
+                        .andEventAvailabilityTimeout(2000, TimeUnit.MILLISECONDS);
+
+        processingConfigurer.registerTrackingEventProcessorConfiguration(config -> tepConfig);
+    }
+
+
+    @Bean
+    public TokenStore axonTokenStore(MongoClient client) {
+        Serializer tokenSerializer = XStreamSerializer.builder().build();
+
+        return MongoTokenStore.builder()
+                .serializer(tokenSerializer)
+                .mongoTemplate(DefaultMongoTemplate.builder().mongoDatabase(client).build())
+                .build();
+
+    }
+
+
+
     @Bean
     public CommandBus registerMessageHandlerInterceptors(PolicyDecisionPoint pdp) {
         log.info("### registering MessageHandler Interceptors");
@@ -44,4 +90,5 @@ public class MyAxonConfiguration {
         commandBus.registerHandlerInterceptor(new PdpCommandInterceptor(pdp));
         return commandBus;
     }
+
 }
