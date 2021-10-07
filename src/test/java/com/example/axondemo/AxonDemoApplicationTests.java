033fc0473ca94266593e400e51bec54b82069309
Dominik Wuttke <Dominik.wu@gmx.net>
2021-10-07 22:37:17 +0200

    fixed failing tests due to missing Mongo Db

--- src/test/java/com/example/axondemo/AxonDemoApplicationTests.java
+++ src/test/java/com/example/axondemo/AxonDemoApplicationTests.java
@@ -1,13 +1,16 @@
 package com.example.axondemo;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-//@SpringBootTest
-//class AxonDemoApplicationTests {
-//
-//    @Test
-//    void contextLoads() {
-//    }
-//
-//}
+@SpringBootTest
+class AxonDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
